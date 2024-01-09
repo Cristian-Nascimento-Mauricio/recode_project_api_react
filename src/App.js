62403b4f2cde534c233cabd4c90f8a55e3969dcd
Memedroid Eventos <123564826+Cristian-Nascimento-Mauricio@users.noreply.github.com>
2024-01-09 20:00:45 -0300

    update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,18 @@ function App() {
       <header>
         <h1>Projeto recode api </h1>
       </header>
+      <div className='containerButton'>
+        <p>Pegar por faixa de preço.</p>
+        <div className='containerInputs'>
+          <p>Mínino:  </p>
+          <input id='lowest' type='number'></input>
+          <p>Máximo: </p>
+          <input id='tallest' type='number'></input>
+        </div>
 
+        <button onClick={updateList}>Okay</button>
+
+      </div>
       <main>
         <table>
           <thead>
@@ -35,18 +46,22 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {list.map(element => (
-              <tr key={element.id}>
-                <td>{element.id}</td>
-                <td>{element.date}</td>
-                <td>{element.boarding}</td>
-                <td>{element.destiny}</td>
-                <td>{element.price}</td>
-                <td>{element.promotion}</td>
-                <td>{element.price * element.promotion}</td>
 
-              </tr>
-            ))}
+            {
+              list.length > 0 ? 
+              list.map(element => (
+                <tr key={element.id}>
+                  <td>{element.id}</td>
+                  <td>{element.date}</td>
+                  <td>{element.boarding}</td>
+                  <td>{element.destiny}</td>
+                  <td>{element.price}</td>
+                  <td>{element.promotion}</td>
+                  <td>{element.price * element.promotion}</td>
+
+                </tr>
+              )) : (<></>)           
+            }
           </tbody>
         </table>
       </main>
@@ -54,6 +69,23 @@ function App() {
     </div>
     
   );
+
+  function updateList(){
+
+    let lowest = document.getElementById('lowest').value 
+    let tallest = document.getElementById('tallest').value 
+
+    console.log(lowest ? 0:lowest,tallest ? 10000:tallest)
+
+    fetch(`http://localhost:5158/api/Values/${lowest ? lowest:0}/${tallest ? tallest:999999}`,{method:"GET"})
+    .then(res => res.json())
+    .then(data => setList(data))
+    .catch(console.log("Erro lista não carregada"))
+
+  }
+
 }
+
+
 
 export default App;
